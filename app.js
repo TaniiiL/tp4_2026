f801283e1afe7a78cf4d5d8ec3f959a0a302b3ba
bf0880-boop <bf0880@est.ort.edu.ar>
2026-08-27 09:09:28 -0300

    faltaescucho

--- app.js
+++ app.js
@@ -6,24 +6,85 @@ const {Client} = pkg;
 const client = new Client(dbconfig)
 await client.connect()
 
-const result = await client.query("SELECT * FROM usuario order by id")
-console.log(result.rows)
-const usuario1 = result.rows[0].nombre
-console.log("usuario1:",usuario1)
-await client.end()
+
 
 app.post('/createuser', async (req, res) => {
   const user = req.body;
-  if (!user.nombre || !user.userid || !user.password) {
-    return res.status(400) .json ({message: "Debe completar todos los campos"});
 
-    try {}
-    
+  if (!user.nombre || !user.userid || !user.password) {
+    return res.status(400).json({
+      message: "Debe completar todos los campos"
+    });
   }
+
+  try {
+    const client = new Client(config);
+
+    await client.connect();
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+
+    const result = await client.query(
+      'INSERT INTO usuario VALUES ($1, $2, $3) RETURNING *',
+      [user.userid, user.nombre, user.password]
+    );
+
+    await client.end();
+
+    console.log("Rows creadas:", result.rowCount);
+
+    res.status(201).json(result.rows);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 
 app.post('/login', async (req, res) => {
   const user = req.body;
-  if (!user.userid || !user.password) 
+
+  if (!user.userid || !user.password) {
+    return res.status(400).json({ message: "Debe completar todos los campos" });
+  }
+
+  try {
+    let result = await client.query(
+      "select id,password from usuario where userid=$1",
+      [user.userid]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    let dbUser = result.rows[0];
+
+    const passOK = await bcrypt.compare(user.password, dbUser.password);
+
+    if (passOK) {
+      const payload = {
+        id:dbUser.id,
+        username:user.username
+      }
+      const secret ='dfkjwedfdj'
+      const token = jwt.sign....
+
+      res.send( token );
+    } else {
+      res.send("Clave invalida");
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+    });
+
+app.post('/escucha', async (req, res) => {
+
 })
 
 const app = express()
